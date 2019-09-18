e6ba515c5403cfa8df7e4caafbf28a1d24fdcf39
shubham ahuja <43359669+shubhahuja@users.noreply.github.com>
2019-09-18 16:52:36 -0400

    keyboard added

---
+++ keyboard2.py
@@ -0,0 +1,407 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'keyboard2.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    text=""
+    
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(660, 351)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.num1 = QtWidgets.QPushButton(self.centralwidget)
+        self.num1.setGeometry(QtCore.QRect(50, 140, 41, 28))
+        self.num1.setObjectName("num1")
+        self.charQ = QtWidgets.QPushButton(self.centralwidget)
+        self.charQ.setGeometry(QtCore.QRect(50, 170, 41, 28))
+        self.charQ.setObjectName("charQ")
+        self.charD = QtWidgets.QPushButton(self.centralwidget)
+        self.charD.setGeometry(QtCore.QRect(150, 200, 41, 28))
+        self.charD.setObjectName("charD")
+        self.num3 = QtWidgets.QPushButton(self.centralwidget)
+        self.num3.setGeometry(QtCore.QRect(150, 140, 41, 28))
+        self.num3.setObjectName("num3")
+        self.charA = QtWidgets.QPushButton(self.centralwidget)
+        self.charA.setGeometry(QtCore.QRect(50, 200, 41, 28))
+        self.charA.setObjectName("charA")
+        self.num7 = QtWidgets.QPushButton(self.centralwidget)
+        self.num7.setGeometry(QtCore.QRect(350, 140, 41, 28))
+        self.num7.setObjectName("num7")
+        self.charF = QtWidgets.QPushButton(self.centralwidget)
+        self.charF.setGeometry(QtCore.QRect(200, 200, 41, 28))
+        self.charF.setObjectName("charF")
+        self.charE = QtWidgets.QPushButton(self.centralwidget)
+        self.charE.setGeometry(QtCore.QRect(150, 170, 41, 28))
+        self.charE.setObjectName("charE")
+        self.charR = QtWidgets.QPushButton(self.centralwidget)
+        self.charR.setGeometry(QtCore.QRect(200, 170, 41, 28))
+        self.charR.setObjectName("charR")
+        self.num4 = QtWidgets.QPushButton(self.centralwidget)
+        self.num4.setGeometry(QtCore.QRect(200, 140, 41, 28))
+        self.num4.setObjectName("num4")
+        self.charS = QtWidgets.QPushButton(self.centralwidget)
+        self.charS.setGeometry(QtCore.QRect(100, 200, 41, 28))
+        self.charS.setObjectName("charS")
+        self.num2 = QtWidgets.QPushButton(self.centralwidget)
+        self.num2.setGeometry(QtCore.QRect(100, 140, 41, 28))
+        self.num2.setObjectName("num2")
+        self.charN = QtWidgets.QPushButton(self.centralwidget)
+        self.charN.setGeometry(QtCore.QRect(300, 230, 41, 28))
+        self.charN.setObjectName("charN")
+        self.charM = QtWidgets.QPushButton(self.centralwidget)
+        self.charM.setGeometry(QtCore.QRect(350, 230, 41, 28))
+        self.charM.setObjectName("charM")
+        self.charP = QtWidgets.QPushButton(self.centralwidget)
+        self.charP.setGeometry(QtCore.QRect(500, 170, 41, 28))
+        self.charP.setObjectName("charP")
+        self.charO = QtWidgets.QPushButton(self.centralwidget)
+        self.charO.setGeometry(QtCore.QRect(450, 170, 41, 28))
+        self.charO.setObjectName("charO")
+        self.num0 = QtWidgets.QPushButton(self.centralwidget)
+        self.num0.setGeometry(QtCore.QRect(500, 140, 41, 28))
+        self.num0.setObjectName("num0")
+        self.num9 = QtWidgets.QPushButton(self.centralwidget)
+        self.num9.setGeometry(QtCore.QRect(450, 140, 41, 28))
+        self.num9.setObjectName("num9")
+        self.num8 = QtWidgets.QPushButton(self.centralwidget)
+        self.num8.setGeometry(QtCore.QRect(400, 140, 41, 28))
+        self.num8.setObjectName("num8")
+        self.charI = QtWidgets.QPushButton(self.centralwidget)
+        self.charI.setGeometry(QtCore.QRect(400, 170, 41, 28))
+        self.charI.setObjectName("charI")
+        self.charK = QtWidgets.QPushButton(self.centralwidget)
+        self.charK.setGeometry(QtCore.QRect(400, 200, 41, 28))
+        self.charK.setObjectName("charK")
+        self.charZ = QtWidgets.QPushButton(self.centralwidget)
+        self.charZ.setGeometry(QtCore.QRect(50, 230, 41, 28))
+        self.charZ.setObjectName("charZ")
+        self.charX = QtWidgets.QPushButton(self.centralwidget)
+        self.charX.setGeometry(QtCore.QRect(100, 230, 41, 28))
+        self.charX.setObjectName("charX")
+        self.charC = QtWidgets.QPushButton(self.centralwidget)
+        self.charC.setGeometry(QtCore.QRect(150, 230, 41, 28))
+        self.charC.setObjectName("charC")
+        self.charV = QtWidgets.QPushButton(self.centralwidget)
+        self.charV.setGeometry(QtCore.QRect(200, 230, 41, 28))
+        self.charV.setObjectName("charV")
+        self.charB = QtWidgets.QPushButton(self.centralwidget)
+        self.charB.setGeometry(QtCore.QRect(250, 230, 41, 28))
+        self.charB.setObjectName("charB")
+        self.charL = QtWidgets.QPushButton(self.centralwidget)
+        self.charL.setGeometry(QtCore.QRect(450, 200, 41, 28))
+        self.charL.setObjectName("charL")
+        self.num5 = QtWidgets.QPushButton(self.centralwidget)
+        self.num5.setGeometry(QtCore.QRect(250, 140, 41, 28))
+        self.num5.setObjectName("num5")
+        self.charT = QtWidgets.QPushButton(self.centralwidget)
+        self.charT.setGeometry(QtCore.QRect(250, 170, 41, 28))
+        self.charT.setObjectName("charT")
+        self.charG = QtWidgets.QPushButton(self.centralwidget)
+        self.charG.setGeometry(QtCore.QRect(250, 200, 41, 28))
+        self.charG.setObjectName("charG")
+        self.num6 = QtWidgets.QPushButton(self.centralwidget)
+        self.num6.setGeometry(QtCore.QRect(300, 140, 41, 28))
+        self.num6.setObjectName("num6")
+        self.charY = QtWidgets.QPushButton(self.centralwidget)
+        self.charY.setGeometry(QtCore.QRect(300, 170, 41, 28))
+        self.charY.setObjectName("charY")
+        self.charH = QtWidgets.QPushButton(self.centralwidget)
+        self.charH.setGeometry(QtCore.QRect(300, 200, 41, 28))
+        self.charH.setObjectName("charH")
+        self.charJ = QtWidgets.QPushButton(self.centralwidget)
+        self.charJ.setGeometry(QtCore.QRect(350, 200, 41, 28))
+        self.charJ.setObjectName("charJ")
+        self.charU = QtWidgets.QPushButton(self.centralwidget)
+        self.charU.setGeometry(QtCore.QRect(350, 170, 41, 28))
+        self.charU.setObjectName("charU")
+        self.charW = QtWidgets.QPushButton(self.centralwidget)
+        self.charW.setGeometry(QtCore.QRect(100, 170, 41, 28))
+        self.charW.setObjectName("charW")
+        self.space = QtWidgets.QPushButton(self.centralwidget)
+        self.space.setGeometry(QtCore.QRect(400, 230, 93, 28))
+        self.space.setObjectName("space")
+        self.enter = QtWidgets.QPushButton(self.centralwidget)
+        self.enter.setGeometry(QtCore.QRect(500, 200, 41, 31))
+        self.enter.setObjectName("enter")
+        self.delB = QtWidgets.QPushButton(self.centralwidget)
+        self.delB.setGeometry(QtCore.QRect(500, 230, 41, 31))
+        self.delB.setObjectName("delB")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(60, 70, 481, 61))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 660, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        
+        self.charA.clicked.connect(self.buttonA)
+        self.charB.clicked.connect(self.buttonB)
+        self.charC.clicked.connect(self.buttonC)
+        self.charD.clicked.connect(self.buttonD)
+        self.charE.clicked.connect(self.buttonE)
+        self.charF.clicked.connect(self.buttonF)
+        self.charG.clicked.connect(self.buttonG)
+        self.charH.clicked.connect(self.buttonH)
+        self.charI.clicked.connect(self.buttonI)
+        self.charJ.clicked.connect(self.buttonJ)
+        self.charK.clicked.connect(self.buttonK)
+        self.charL.clicked.connect(self.buttonL)
+        self.charM.clicked.connect(self.buttonM)
+        self.charN.clicked.connect(self.buttonN)
+        self.charO.clicked.connect(self.buttonO)
+        self.charP.clicked.connect(self.buttonP)
+        self.charQ.clicked.connect(self.buttonQ)
+        self.charR.clicked.connect(self.buttonR)
+        self.charS.clicked.connect(self.buttonS)
+        self.charT.clicked.connect(self.buttonT)
+        self.charU.clicked.connect(self.buttonU)
+        self.charV.clicked.connect(self.buttonV)
+        self.charW.clicked.connect(self.buttonW)
+        self.charZ.clicked.connect(self.buttonZ)
+        self.charY.clicked.connect(self.buttonY)
+        self.charZ.clicked.connect(self.buttonZ)
+        self.num1.clicked.connect(self.button1)
+        self.num2.clicked.connect(self.button2)
+        self.num3.clicked.connect(self.button3)
+        self.num4.clicked.connect(self.button4)
+        self.num5.clicked.connect(self.button5)
+        self.num6.clicked.connect(self.button6)
+        self.num7.clicked.connect(self.button7)
+        self.num8.clicked.connect(self.button8)
+        self.num9.clicked.connect(self.button9)
+        self.num0.clicked.connect(self.button0)
+        self.space.clicked.connect(self.buttonspace)
+
+        self.delB.clicked.connect(self.buttondel)
+        
+
+    def retranslateUi(self, MainWindow):
+        color="red"
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.num1.setText(_translate("MainWindow", "1"))
+        self.charQ.setText(_translate("MainWindow", "q"))
+        self.charD.setText(_translate("MainWindow", "d"))
+        self.num3.setText(_translate("MainWindow", "3"))
+        self.charA.setText(_translate("MainWindow", "a"))
+        self.num7.setText(_translate("MainWindow", "7"))
+        self.charF.setText(_translate("MainWindow", "f"))
+        self.charE.setText(_translate("MainWindow", "e"))
+        self.charR.setText(_translate("MainWindow", "r"))
+        self.num4.setText(_translate("MainWindow", "4"))
+        self.charS.setText(_translate("MainWindow", "s"))
+        self.num2.setText(_translate("MainWindow", "2"))
+        self.charN.setText(_translate("MainWindow", "n"))
+        self.charM.setText(_translate("MainWindow", "m"))
+        self.charP.setText(_translate("MainWindow", "p"))
+        self.charO.setText(_translate("MainWindow", "o"))
+        self.num0.setText(_translate("MainWindow", "0"))
+        self.num9.setText(_translate("MainWindow", "9"))
+        self.num8.setText(_translate("MainWindow", "8"))
+        self.charI.setText(_translate("MainWindow", "i"))
+        self.charK.setText(_translate("MainWindow", "k"))
+        self.charZ.setText(_translate("MainWindow", "z"))
+        self.charX.setText(_translate("MainWindow", "x"))
+        self.charC.setText(_translate("MainWindow", "c"))
+        self.charV.setText(_translate("MainWindow", "v"))
+        self.charB.setText(_translate("MainWindow", "b"))
+        self.charL.setText(_translate("MainWindow", "l"))
+        self.num5.setText(_translate("MainWindow", "5"))
+        self.charT.setText(_translate("MainWindow", "t"))
+        self.charG.setText(_translate("MainWindow", "g"))
+        self.num6.setText(_translate("MainWindow", "6"))
+        self.charY.setText(_translate("MainWindow", "y"))
+        self.charH.setText(_translate("MainWindow", "h"))
+        self.charJ.setText(_translate("MainWindow", "j"))
+        self.charU.setText(_translate("MainWindow", "u"))
+        self.charW.setText(_translate("MainWindow", "w"))
+        self.space.setText(_translate("MainWindow", "space"))
+        self.enter.setText(_translate("MainWindow", "enter"))
+        self.delB.setText(_translate("MainWindow", "del"))
+        self.label.setText(_translate("MainWindow", "Enter Text here"))
+
+
+
+
+
+    def buttonA(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"a"
+        self.label.setText(Ui_MainWindow.text)
+    def buttonB(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"b"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonC(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"c"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonD(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"d"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonE(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"e"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonF(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"f"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonG(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"g"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonH(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"h"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonI(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"i"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonJ(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"j"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonK(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"k"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonL(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"l"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonM(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"m"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonN(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"n"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonO(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"o"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonP(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"p"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonQ(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"q"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonR(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"r"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonS(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"s"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonT(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"t"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonU(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"u"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonV(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"v"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonW(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"w"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonZ(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"z"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonY(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"y"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonZ(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"z"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button1(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"1"
+        self.label.setText(Ui_MainWindow.text)
+
+
+    def button2(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"2"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button3(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"3"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button4(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"4"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button5(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"5"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button6(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"6"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button7(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"7"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button8(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"8"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button9(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"9"
+        self.label.setText(Ui_MainWindow.text)
+
+    def button0(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+"0"
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttonspace(self):
+        Ui_MainWindow.text=Ui_MainWindow.text+" "
+        self.label.setText(Ui_MainWindow.text)
+
+    def buttondel(self):
+        if len(Ui_MainWindow.text)==0:
+            pass
+        else:
+            Ui_MainWindow.text=Ui_MainWindow.text[:-1]
+            self.label.setText(Ui_MainWindow.text)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
